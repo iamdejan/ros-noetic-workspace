d1079db6d7b37cc056cc6358e84acc3b437bf996
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-03-19 23:02:13 +0800

    feat(rosrust-controller): add simple publisher as an example of rosrust library capability

---
+++ src/rosrust_controller/src/main.rs
@@ -0,0 +1,23 @@
+const NODE_NAME: &'static str = "rosrust_rusty_talker";
+
+fn main() {
+    env_logger::init();
+    rosrust::init(NODE_NAME);
+    rosrust::ros_info!("Node has been started");
+
+    let publisher = rosrust::publish::<rosrust_msg::std_msgs::String>("/robot_news_radio", 10).unwrap();
+    publisher.wait_for_subscribers(None).unwrap();
+
+    let rate = rosrust::rate(5.0);
+    while rosrust::is_ok() {
+        let message = rosrust_msg::std_msgs::String {
+            data: format!("Hi, this is {} node speaking!", NODE_NAME),
+        };
+        rosrust::ros_info!("Sending message...");
+        publisher.send(message).unwrap();
+
+        rate.sleep();
+    }
+
+    rosrust::spin();
+}
