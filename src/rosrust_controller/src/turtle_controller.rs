fb0ccdd3f69071ddbd7940631ab96b5d3fc4c46a
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-03-22 14:06:17 +0800

    refactor(rosrust-controller): move callback into its own function

--- src/rosrust_controller/src/turtle_controller.rs
+++ src/rosrust_controller/src/turtle_controller.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
 
+use rosrust::Publisher;
 use rosrust_msg::geometry_msgs;
 use rosrust_msg::turtlesim;
 
@@ -20,6 +21,33 @@ fn call_set_pen_service(r: u8, g: u8, b: u8, width: u8, off: u8) {
     client.req(&command).unwrap().unwrap();
 }
 
+fn pose_callback(
+    pose: turtlesim::Pose,
+    publisher: &Publisher<geometry_msgs::Twist>,
+    previous_x: &Arc<Mutex<f32>>,
+) {
+    let mut command = geometry_msgs::Twist::default();
+    command.linear.x = 5.0;
+    command.angular.z = 0.0;
+
+    if pose.x < 2.0 || pose.x > 9.0 || pose.y > 9.0 || pose.y < 2.0 {
+        command.linear.x = 1.0;
+        command.angular.z = 1.4;
+    }
+
+    let previous_x_val = *previous_x.lock().unwrap();
+
+    if pose.x >= 5.5 && previous_x_val < 5.5 {
+        call_set_pen_service(255, 0, 0, 3, 0);
+    } else if pose.x < 5.5 && previous_x_val >= 5.5 {
+        call_set_pen_service(0, 255, 0, 3, 0);
+    }
+
+    *previous_x.lock().unwrap() = pose.x;
+
+    publisher.send(command).unwrap();
+}
+
 fn main() {
     env_logger::init();
     rosrust::init(NODE_NAME);
@@ -29,26 +57,7 @@ fn main() {
 
     let publisher = rosrust::publish::<geometry_msgs::Twist>("/turtle1/cmd_vel", 10).unwrap();
     let _subscriber = rosrust::subscribe("/turtle1/pose", 10, move |pose: turtlesim::Pose| {
-        let mut command = geometry_msgs::Twist::default();
-        command.linear.x = 5.0;
-        command.angular.z = 0.0;
-
-        if pose.x < 2.0 || pose.x > 9.0 || pose.y > 9.0 || pose.y < 2.0 {
-            command.linear.x = 1.0;
-            command.angular.z = 1.4;
-        }
-
-        let previous_x_val = *previous_x.lock().unwrap();
-
-        if pose.x >= 5.5 && previous_x_val < 5.5 {
-            call_set_pen_service(255, 0, 0, 3, 0);
-        } else if pose.x < 5.5 && previous_x_val >= 5.5 {
-            call_set_pen_service(0, 255, 0, 3, 0);
-        }
-
-        *previous_x.lock().unwrap() = pose.x;
-
-        publisher.send(command).unwrap();
+        pose_callback(pose, &publisher, &previous_x);
     })
     .unwrap();
 
