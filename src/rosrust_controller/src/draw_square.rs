be66d03d91db5a1538568e8a99e88e428e3d6987
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-03-26 23:47:30 +0800

    finish callback implementation

--- src/rosrust_controller/src/draw_square.rs
+++ src/rosrust_controller/src/draw_square.rs
@@ -3,18 +3,106 @@ use rosrust_msg::geometry_msgs;
 use rosrust_msg::turtlesim;
 
 const NODE_NAME: &str = "rosrust_draw_square";
-const THETA_TARGET: [f64; 4] = [0.0, 90_f64.to_radians(),-180_f64.to_radians(), (270.0_f64-360.0_f64).to_radians()];
 
-fn callback(
-    pose: &turtlesim::Pose,
-    publisher: &Publisher<geometry_msgs::Twist>
-) {
-    let mut command = geometry_msgs::Twist::default();
-    let theta_target = THETA_TARGET[0];
-    _ = pose;
-    _ = theta_target;
+#[derive(PartialEq, Eq)]
+enum State {
+    Still,
+    Forward,
+    TurnLeft,
+}
 
-    publisher.send(command).unwrap();
+struct Node {
+    publisher: Publisher<geometry_msgs::Twist>,
+    current_state: State,
+    current_target_index: usize,
+    x_limits: [f64; 2],
+    y_limits: [f64; 2],
+}
+
+impl Node {
+    const DELTA: f64 = 1e-2;
+    const THETA_TARGETS: [f64; 4] = [
+        0.0,
+        90_f64.to_radians(),
+        -180_f64.to_radians(),
+        (270.0_f64 - 360.0_f64).to_radians(),
+    ];
+    const FORWARD_SPEED: f64 = 4.0;
+    const TURN_SPEED: f64 = 0.2;
+    const UPDATE_BY_DELTA_MULTIPLIER: f64 = 0.0;
+
+    fn new(publisher: Publisher<geometry_msgs::Twist>) -> Self {
+        return Self {
+            publisher: publisher,
+            current_state: State::Still,
+            current_target_index: 0,
+            x_limits: [2.0, 9.0],
+            y_limits: [2.0, 9.0],
+        };
+    }
+
+    fn need_to_turn_left(&self, pose: &turtlesim::Pose) -> bool {
+        if (pose.theta as f64 - Self::THETA_TARGETS[0]).abs() <= Self::DELTA
+            || (pose.theta as f64 - Self::THETA_TARGETS[2]).abs() > Self::DELTA
+        {
+            return (pose.x as f64) < self.x_limits[0] || (pose.x as f64) > self.x_limits[1];
+        }
+
+        if (pose.theta as f64 - Self::THETA_TARGETS[1]).abs() <= Self::DELTA
+            || (pose.theta as f64 - Self::THETA_TARGETS[3]).abs() > Self::DELTA
+        {
+            return (pose.y as f64) < self.y_limits[0] || (pose.y as f64) > self.y_limits[1];
+        }
+
+        return false;
+    }
+
+    fn update_limits(&mut self, pose: &turtlesim::Pose) {
+        self.x_limits[0] =
+            self.x_limits[0].min((pose.x as f64) - Self::UPDATE_BY_DELTA_MULTIPLIER * Self::DELTA);
+        self.x_limits[1] =
+            self.x_limits[1].max((pose.x as f64) + Self::UPDATE_BY_DELTA_MULTIPLIER * Self::DELTA);
+
+        self.y_limits[0] =
+            self.y_limits[0].min((pose.y as f64) - Self::UPDATE_BY_DELTA_MULTIPLIER * Self::DELTA);
+        self.y_limits[1] =
+            self.y_limits[1].max((pose.y as f64) + Self::UPDATE_BY_DELTA_MULTIPLIER * Self::DELTA);
+    }
+
+    fn callback(&mut self, pose: &turtlesim::Pose) {
+        let mut command = geometry_msgs::Twist::default();
+        let theta_target = Self::THETA_TARGETS[self.current_target_index];
+
+        if self.current_state == State::Still
+            && (pose.theta as f64 - theta_target).abs() <= Self::DELTA
+        {
+            command.linear.x = Self::FORWARD_SPEED;
+            command.angular.z = 0.0;
+            self.current_state = State::Forward;
+        } else if self.current_state == State::Forward && self.need_to_turn_left(pose) {
+            command.linear.x = 0.0;
+            self.current_state = State::Still;
+            self.current_target_index = (self.current_target_index + 1) % Self::THETA_TARGETS.len();
+        } else if self.current_state == State::Still
+            && (pose.theta as f64 - theta_target).abs() > Self::DELTA
+        {
+            self.update_limits(pose);
+
+            command.angular.z = Self::TURN_SPEED;
+            self.current_state = State::TurnLeft;
+        } else if self.current_state == State::TurnLeft
+            && (pose.theta as f64 - theta_target).abs() <= Self::DELTA
+        {
+            command.linear.z = 0.0;
+            self.current_state = State::Still;
+        } else if self.current_state == State::Forward {
+            command.linear.x = Self::FORWARD_SPEED;
+        } else if self.current_state == State::TurnLeft {
+            command.angular.z = Self::TURN_SPEED;
+        }
+
+        self.publisher.send(command).unwrap();
+    }
 }
 
 fn main() {
@@ -22,6 +110,7 @@ fn main() {
     rosrust::init(NODE_NAME);
 
     let publisher = rosrust::publish::<geometry_msgs::Twist>("/turtle1/cmd_vel", 10).unwrap();
+    let mut node = Node::new(publisher);
 
     // Save to a variable, even though we don't use it.
     // This is due to Rust RAII (Resource Acquisition Is Initialization) mechanism,
@@ -30,7 +119,7 @@ fn main() {
     // the variable goes out of scope.
     // Similar mechanism happens with C++.
     let _subscriber = rosrust::subscribe("/turtle1/pose", 10, move |pose: turtlesim::Pose| {
-        callback(&pose, &publisher);
+        node.callback(&pose);
     })
     .unwrap();
 
