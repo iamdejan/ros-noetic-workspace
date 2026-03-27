4d5e7845f44caabd0ab0227f96ba8fac7818022b
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-03-27 22:27:44 +0800

    feat(actionlib-basics): implement action server and client

---
+++ src/actionlib_basics/src/fibonacci_server.cpp
@@ -0,0 +1,67 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib_basics/FibonacciAction.h>
+
+class FibonacciAction {
+protected:
+    ros::NodeHandle nh = ros::NodeHandle();
+
+    // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+    actionlib::SimpleActionServer<actionlib_basics::FibonacciAction> action_server;
+
+    std::string action_name;
+
+    actionlib_basics::FibonacciFeedback feedback;
+    actionlib_basics::FibonacciResult result;
+public:
+    FibonacciAction(std::string name) :
+        action_server(nh, name, boost::bind(&FibonacciAction::executeCallback, this, _1), false),
+        action_name(name) {
+            action_server.start();
+        }
+    
+    ~FibonacciAction(void) {}
+
+    void executeCallback(const actionlib_basics::FibonacciGoalConstPtr &goal) {
+        // helper variables
+        auto rate = ros::Rate(1);
+        bool success = true;
+
+        // push back the seeds for the Fibonacci sequence
+        feedback.sequence.clear();
+        feedback.sequence.push_back(0);
+        feedback.sequence.push_back(1);
+
+        // publish info to the console for the user
+        ROS_INFO("%s: Executing, creating Fibonacci sequence of order %i with seeds %i, %i", action_name.c_str(), goal->order, feedback.sequence[0], feedback.sequence[1]);
+    
+        // start executing the action
+        for (int i = 1; i <= goal->order; i++) {
+            if (action_server.isPreemptRequested() || !ros::ok()) {
+                ROS_INFO("%s: Preempted", action_name.c_str());
+                action_server.setPreempted();
+                success = false;
+                break;
+            }
+
+            feedback.sequence.push_back(feedback.sequence[i] +feedback.sequence[i - 1]);
+
+            action_server.publishFeedback(feedback);
+
+            rate.sleep();
+        }
+
+        if (success) {
+            result.sequence = feedback.sequence;
+            ROS_INFO("%s: Succeeded", action_name.c_str());
+            action_server.setSucceeded(result);
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "fibonacci");
+
+    auto fibonacci = FibonacciAction("fibonacci");
+    ros::spin();
+}
