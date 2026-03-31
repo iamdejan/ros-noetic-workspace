236e6ba5aaa98d2755d7d698df7b1bb6131aa45f
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-03-31 16:09:36 +0800

    implement turtle tf2 broadcaster

---
+++ src/learning_tf2/src/turtle_tf2_broadcaster.cpp
@@ -0,0 +1,67 @@
+#include <string>
+
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <turtlesim/Pose.h>
+
+class Node {
+private:
+    // Create a TransformBroadcaster object that
+    // we will use to send the transformations.
+    inline static tf2_ros::TransformBroadcaster broadcaster;
+
+    std::string turtle_name;
+public:
+    Node(std::string turtle_name) : turtle_name(turtle_name) {}
+
+    void poseCallback(turtlesim::Pose message) {
+        // Create a TransformStamped object that
+        // we will use to send the transformations.
+        auto transform_stamped = geometry_msgs::TransformStamped();
+
+        // Initialize the transform_stamped object with some data
+        transform_stamped.header.stamp = ros::Time::now();
+        transform_stamped.header.frame_id = "world";
+        transform_stamped.child_frame_id = this->turtle_name;
+
+        // Copy transformation from 3D turtle post to 3D transform
+        transform_stamped.transform.translation.x = message.x;
+        transform_stamped.transform.translation.y = message.y;
+        transform_stamped.transform.translation.z = 0.0;
+
+        auto quaternion = tf2::Quaternion();
+        quaternion.setRPY(0, 0, message.theta);
+        transform_stamped.transform.rotation.x = quaternion.x();
+        transform_stamped.transform.rotation.y = quaternion.y();
+        transform_stamped.transform.rotation.z = quaternion.z();
+        transform_stamped.transform.rotation.w = quaternion.w();
+
+        // Send the transform
+        broadcaster.sendTransform(transform_stamped);
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "my_tf2_broadcaster");
+
+    std::string turtle_name;
+
+    auto private_node = ros::NodeHandle("~");
+    if (!private_node.hasParam("turtle")) {
+        if (argc != 2) {
+            ROS_ERROR("need turtle name as argument");
+            return -1;
+        }
+
+        turtle_name = argv[1];
+    } else {
+        private_node.getParam("turtle", turtle_name);
+    }
+
+    auto nh = ros::NodeHandle();
+    auto node = Node(turtle_name);
+    auto subscriber = nh.subscribe(turtle_name+"/pose", 10, &Node::poseCallback, &node);
+
+    ros::spin();
+}
